87b11c1833cad09d82a732d3c3ded4d5ded2b6aa
trescube <stephen.k.hess@gmail.com>
2018-07-11 00:39:54 -0400

    refactoring

--- index.js
+++ index.js
@@ -12,113 +12,8 @@ const supportedFieldTypes = new Set(['C', 'D', 'F', 'L', 'M', 'N']);
 const truthyValues = new Set(['Y', 'y', 'T', 't']);
 const falseyValues = new Set(['N', 'n', 'F', 'f']);
 
-// valid M-type value regex
+// valid M-type value regex (10 digits or 10 spaces)
 const validMTypeValueRegex = /^(\d{10}| {10})$/;
-
-function parseHeaderField(fieldBytes, val, i) {
-  const field = fieldBytes.slice(i*32, i*32+32);
-
-  const length = field.readUInt8(16);
-  if (length === 255) {
-    throw new Error('Field length must be less than 255');
-  }
-
-  const type = field.toString('utf-8', 11, 12);
-  if (!supportedFieldTypes.has(type)) {
-    throw new Error(`Field type must be one of: ${Array.from(supportedFieldTypes).join(', ')}`);
-  }
-
-  // check types & lengths
-  if (type === 'D' && length !== 8) {
-    throw new Error(`Invalid D (date) field length: ${length}`);
-  }
-  if (type === 'L' && length !== 1) {
-    throw new Error(`Invalid L (logical) field length: ${length}`);
-  }
-  if (type === 'M' && length !== 10) {
-    throw new Error(`Invalid M (memo) field length: ${length}`);
-  }
-
-  const isIndexedInMDXFile = field.readUInt8(31);
-  if (isIndexedInMDXFile > 1) {
-    throw new Error(`Invalid indexed in production MDX file value: ${isIndexedInMDXFile}`);
-  }
-
-  return {
-    name: field.toString('utf-8', 0, 10).replace(/\0/g, ''),
-    type: type,
-    length: length,
-    precision: field.readUInt8(17),
-    workAreaId: field.readUInt16LE(18),
-    isIndexedInMDXFile: isIndexedInMDXFile === 1
-  };
-
-}
-
-function parseHeader(chunk) {
-  const versionByte = chunk.readUInt8(0);
-  if (!supportedVersions.has(versionByte)) {
-    throw new Error(`Unsupported version: ${versionByte}`);
-  }
-
-  const numberOfHeaderBytes = chunk.readUInt16LE(8);
-  // the number of header bytes should be 1 when modded with 32
-  if (numberOfHeaderBytes % 32 !== 1) {
-    throw new Error(`Invalid number of header bytes: ${numberOfHeaderBytes}`);
-  }
-
-  // there are 32 bytes per header field + 1 byte for terminator + 32 bytes for the initial header
-  const numberOfFields = (numberOfHeaderBytes-32-1)/32;
-
-  const fieldBytes = chunk.slice(32, numberOfHeaderBytes);
-  // emit an error if the header bytes does not end with 0x0D (per spec)
-  if (fieldBytes.readUInt8(numberOfHeaderBytes-32-1) !== 0x0D) {
-    throw new Error(`Invalid field descriptor array terminator at byte ${numberOfHeaderBytes}`);
-  }
-
-  const encryptionByte = chunk.readUInt8(15);
-  // if the source is encrypted, then emit an error
-  if (encryptionByte === 1) {
-    throw new Error('Encryption flag is set, cannot process');
-  }
-  // valid values for the encryption byte are 0x00 and 0x01, emit an error otherwise
-  if (encryptionByte > 1) {
-    throw new Error(`Invalid encryption flag value: ${encryptionByte}`);
-  }
-
-  const hasProductionMDXFile = chunk.readUInt8(28);
-  // production MDX file existence value must be 0x01 or 0x02 (per spec)
-  if (hasProductionMDXFile > 1) {
-    throw new Error(`Invalid production MDX file existence value: ${hasProductionMDXFile}`);
-  }
-
-  // construct and return the header
-  const header = {
-    version: versionByte,
-    dateOfLastUpdate: new Date(
-      1900 + chunk.readUInt8(1),
-      chunk.readUInt8(2) - 1,
-      chunk.readUInt8(3)
-    ),
-    numberOfRecords: chunk.readInt32LE(4),
-    numberOfHeaderBytes: numberOfHeaderBytes,
-    numberOfBytesInRecord: chunk.readInt16LE(10),
-    hasProductionMDXFile: hasProductionMDXFile,
-    langaugeDriverId: chunk.readUInt8(29),
-    fields: Array.from( {length: numberOfFields }, parseHeaderField.bind(null, fieldBytes))
-  };
-
-  // if there are any duplicate field names, throw an error
-  header.fields.reduce((allFieldNames, field) => {
-    if (allFieldNames.has(field.name)) {
-      throw new Error(`Duplicate field name '${field.name}'`);
-    } 
-    return allFieldNames.add(field.name);
-  }, new Set());
-
-  return header;
-
-}
 
 // type handlers keyed by the single character type designator
 const typeHandlers = {
@@ -156,114 +51,10 @@ const typeHandlers = {
   }
 };
 
-function convertToObject(header, chunk) {
-  const record = {
-    '@meta': {
-      deleted: isDeleted(chunk)
-    }
-  };
-
-  // keep track of how far we're into the record byte-wise
-  // start at 1 since the 0th byte is the deleted flag
-  let byteOffset = 1;
-
-  header.fields.forEach(field => {
-    // read the value out as UTF-8
-    const value = chunk.toString('utf-8', byteOffset, byteOffset+field.length);
-
-    // assign the field into the record
-    record[field.name] = typeHandlers[field.type](value);
-
-    // update where the next field starts
-    byteOffset += field.length;
-
-  });
-
-  return record;
-
-}
-
-function isDeleted(chunk) {
-  const firstByte = chunk.readUInt8(0, 1);
-
-  if (firstByte === 0x20) { // ' '
-    return false;
-  }
-  if (firstByte === 0x2A) { // '*'
-    return true;
-  }
-
-  throw `Invalid deleted record value: ${String.fromCharCode(firstByte)}`;
-
-}
-
-function validateOffset(offset) {
-  if (offset === undefined) {
-    return 0;
-  }
-
-  if (offset < 0 || !Number.isInteger(offset)) {
-    throw new Error('offset must be a non-negative integer');
-  }
-
-  return offset;
-
-}
-
-function validateSize(size) {
-  if (size === undefined) {
-    return Infinity;
-  }
-
-  if (size < 0 || !Number.isInteger(size)) {
-    throw new Error('size must be a non-negative integer');
-  }
-
-  return size;
-
-}
-
-function validateDeleted(deleted) {
-  if (deleted === undefined) {
-    return false;
-  }
-
-  if (deleted !== true && deleted !== false) {
-    throw new Error('deleted must be a boolean');
-  }
-
-  return deleted;
-
-}
-
 module.exports = (options = {}) => {
   const offset = validateOffset(options.offset);
   const size = validateSize(options.size);
   const includeDeletedRecords = validateDeleted(options.deleted);
-
-  function hasEnoughBytesForHeader(chunk) {
-    return chunk.length < 32 || chunk.length < chunk.readUInt16LE(8);
-  }
-
-  function isEligbleForOutput(record) {
-    return !record['@meta'].deleted || includeDeletedRecords;
-  }
-
-  function isWithinPage(count) {
-    return count >= offset && count < offset + size;
-  }
-
-  function allRecordsHaveBeenProcessed(expectedNumberOfRecords, numberOfRecordsProcessed) {
-    return expectedNumberOfRecords === numberOfRecordsProcessed;
-  }
-
-  function aSingleByteRemains(unconsumedBytes) {
-    return unconsumedBytes.length === 1;
-  }
-
-  function firstByteIsEOFMarker(unconsumedBytes) {
-    return unconsumedBytes.readUInt8(0) === 0x1A;
-  }
 
   return new Transform({
     readableObjectMode: true,
@@ -286,14 +77,14 @@ module.exports = (options = {}) => {
 
         // if there aren't enough bytes to read the header, save off the accumulated
         //  bytes for later use
-        if (hasEnoughBytesForHeader(chunk)) {
+        if (!hasEnoughBytesForHeader(chunk)) {
           this.unconsumedBytes = chunk;
           return callback();
         }
 
         try {
           this.header = parseHeader(chunk);
-   
+
           // emit the header for outside consumption
           this.emit('header', this.header);
 
@@ -323,12 +114,9 @@ module.exports = (options = {}) => {
       const numberOfRecordsInThisChunk = Math.floor(chunk.length / this.header.numberOfBytesInRecord);
 
       // slice up the chunk into record-size bites, then iterate and push
-      Array.from({length: numberOfRecordsInThisChunk}, (val, i) => chunk.slice(
-          i * this.header.numberOfBytesInRecord, 
-          i * this.header.numberOfBytesInRecord + this.header.numberOfBytesInRecord)
-      ).forEach(chunk => {
+      Array.from({length: numberOfRecordsInThisChunk}, sliceIntoChunks.bind(this, chunk)).forEach(chunk => {
         try {
-          const record = convertToObject(this.header, chunk);
+          const record = convertChunkToRecord(this.header, chunk);
 
           // only push if it's eligble for output and within the pagination params
           if (isEligbleForOutput(record)) {
@@ -352,7 +140,7 @@ module.exports = (options = {}) => {
       this.unconsumedBytes = chunk.slice(this.header.numberOfBytesInRecord * numberOfRecordsInThisChunk);
 
       // if all the records have been emitted, proceed with shutdown
-      if (allRecordsHaveBeenProcessed(this.header.numberOfRecords, this.totalRecordCount) && 
+      if (allRecordsHaveBeenProcessed(this.header.numberOfRecords, this.totalRecordCount) &&
           aSingleByteRemains(this.unconsumedBytes)) {
 
         // throw an error if the last byte isn't the expected EOF marker
@@ -368,4 +156,224 @@ module.exports = (options = {}) => {
       callback();
     }
   });
+
+  function sliceIntoChunks(chunk, val, i) {
+    return chunk.slice(
+      i * this.header.numberOfBytesInRecord,
+      i * this.header.numberOfBytesInRecord + this.header.numberOfBytesInRecord)
+  }
+
+  // returns true if enough bytes have been read to parse the entire header
+  function hasEnoughBytesForHeader(chunk) {
+    return chunk.length >= 32 && chunk.length >= chunk.readUInt16LE(8);
+  }
+
+  // returns true if record is not deleted or deleted records should be included
+  function isEligbleForOutput(record) {
+    return !record['@meta'].deleted || includeDeletedRecords;
+  }
+
+  // returns true if count is within the page size/offset combination
+  function isWithinPage(count) {
+    return count >= offset && count < offset + size;
+  }
+
+  // returns true if the actual number of records processed matches the number of expected records
+  function allRecordsHaveBeenProcessed(expectedNumberOfRecords, numberOfRecordsProcessed) {
+    return expectedNumberOfRecords === numberOfRecordsProcessed;
+  }
+
+  // returns true if there's exactly 1 byte in a buffer
+  function aSingleByteRemains(unconsumedBytes) {
+    return unconsumedBytes.length === 1;
+  }
+
+  // returns true if the first byte of a buffer is the EOF marker
+  function firstByteIsEOFMarker(unconsumedBytes) {
+    return unconsumedBytes.readUInt8(0) === 0x1A;
+  }
+
+  // convert the buffer to a header object
+  function parseHeader(buffer) {
+    const versionByte = buffer.readUInt8(0);
+    if (!supportedVersions.has(versionByte)) {
+      throw new Error(`Unsupported version: ${versionByte}`);
+    }
+
+    const numberOfHeaderBytes = buffer.readUInt16LE(8);
+    // the number of header bytes should be 1 when modded with 32
+    if (numberOfHeaderBytes % 32 !== 1) {
+      throw new Error(`Invalid number of header bytes: ${numberOfHeaderBytes}`);
+    }
+
+    // there are 32 bytes per header field + 1 byte for terminator + 32 bytes for the initial header
+    const numberOfFields = (numberOfHeaderBytes-32-1)/32;
+
+    const fieldBytes = buffer.slice(32, numberOfHeaderBytes);
+    // emit an error if the header bytes does not end with 0x0D (per spec)
+    if (fieldBytes.readUInt8(numberOfHeaderBytes-32-1) !== 0x0D) {
+      throw new Error(`Invalid field descriptor array terminator at byte ${numberOfHeaderBytes}`);
+    }
+
+    const encryptionByte = buffer.readUInt8(15);
+    // if the source is encrypted, then emit an error
+    if (encryptionByte === 1) {
+      throw new Error('Encryption flag is set, cannot process');
+    }
+    // valid values for the encryption byte are 0x00 and 0x01, emit an error otherwise
+    if (encryptionByte > 1) {
+      throw new Error(`Invalid encryption flag value: ${encryptionByte}`);
+    }
+
+    const hasProductionMDXFile = buffer.readUInt8(28);
+    // production MDX file existence value must be 0x01 or 0x02 (per spec)
+    if (hasProductionMDXFile > 1) {
+      throw new Error(`Invalid production MDX file existence value: ${hasProductionMDXFile}`);
+    }
+
+    // construct and return the header
+    const header = {
+      version: versionByte,
+      dateOfLastUpdate: new Date(
+        1900 + buffer.readUInt8(1),
+        buffer.readUInt8(2) - 1,
+        buffer.readUInt8(3)
+      ),
+      numberOfRecords: buffer.readInt32LE(4),
+      numberOfHeaderBytes: numberOfHeaderBytes,
+      numberOfBytesInRecord: buffer.readInt16LE(10),
+      hasProductionMDXFile: hasProductionMDXFile,
+      langaugeDriverId: buffer.readUInt8(29),
+      fields: Array.from( {length: numberOfFields }, parseHeaderField.bind(null, fieldBytes))
+    };
+
+    // if there are any duplicate field names, throw an error
+    header.fields.reduce((allFieldNames, field) => {
+      if (allFieldNames.has(field.name)) {
+        throw new Error(`Duplicate field name '${field.name}'`);
+      }
+      return allFieldNames.add(field.name);
+    }, new Set());
+
+    return header;
+  }
+
+  function parseHeaderField(fieldBytes, val, i) {
+    const field = fieldBytes.slice(i*32, i*32+32);
+
+    const length = field.readUInt8(16);
+    if (length === 255) {
+      throw new Error('Field length must be less than 255');
+    }
+
+    const type = field.toString('utf-8', 11, 12);
+    if (!supportedFieldTypes.has(type)) {
+      throw new Error(`Field type must be one of: ${Array.from(supportedFieldTypes).join(', ')}`);
+    }
+
+    // check types & lengths
+    if (type === 'D' && length !== 8) {
+      throw new Error(`Invalid D (date) field length: ${length}`);
+    }
+    if (type === 'L' && length !== 1) {
+      throw new Error(`Invalid L (logical) field length: ${length}`);
+    }
+    if (type === 'M' && length !== 10) {
+      throw new Error(`Invalid M (memo) field length: ${length}`);
+    }
+
+    const isIndexedInMDXFile = field.readUInt8(31);
+    if (isIndexedInMDXFile > 1) {
+      throw new Error(`Invalid indexed in production MDX file value: ${isIndexedInMDXFile}`);
+    }
+
+    return {
+      name: field.toString('utf-8', 0, 10).replace(/\0/g, ''),
+      type: type,
+      length: length,
+      precision: field.readUInt8(17),
+      workAreaId: field.readUInt16LE(18),
+      isIndexedInMDXFile: isIndexedInMDXFile === 1
+    };
+  }
+
+  function convertChunkToRecord(header, chunk) {
+    const record = {
+      '@meta': {
+        deleted: isDeleted(chunk)
+      }
+    };
+
+    // keep track of how far we're into the record byte-wise
+    // start at 1 since the 0th byte is the deleted flag
+    let byteOffset = 1;
+
+    header.fields.forEach(field => {
+      // read the value out as UTF-8
+      const value = chunk.toString('utf-8', byteOffset, byteOffset+field.length);
+
+      // assign the field into the record
+      record[field.name] = typeHandlers[field.type](value);
+
+      // update where the next field starts
+      byteOffset += field.length;
+
+    });
+
+    return record;
+  }
+
+  function isDeleted(chunk) {
+    const firstByte = chunk.readUInt8(0, 1);
+
+    if (firstByte === 0x20) { // ' '
+      return false;
+    }
+    if (firstByte === 0x2A) { // '*'
+      return true;
+    }
+
+    throw `Invalid deleted record value: ${String.fromCharCode(firstByte)}`;
+
+  }
+
+  function validateOffset(offset) {
+    if (offset === undefined) {
+      return 0;
+    }
+
+    if (offset < 0 || !Number.isInteger(offset)) {
+      throw new Error('offset must be a non-negative integer');
+    }
+
+    return offset;
+
+  }
+
+  function validateSize(size) {
+    if (size === undefined) {
+      return Infinity;
+    }
+
+    if (size < 0 || !Number.isInteger(size)) {
+      throw new Error('size must be a non-negative integer');
+    }
+
+    return size;
+
+  }
+
+  function validateDeleted(deleted) {
+    if (deleted === undefined) {
+      return false;
+    }
+
+    if (deleted !== true && deleted !== false) {
+      throw new Error('deleted must be a boolean');
+    }
+
+    return deleted;
+
+  }
+
 };
